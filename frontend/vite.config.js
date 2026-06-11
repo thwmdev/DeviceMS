7f21701c973ef0662687123f182e724713bcdcde
thwm-fe <htham5350@gmail.com>
2026-06-11 15:06:43 +0700

    fixed logging default

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+
+    outDir: 'dist',
+
+
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    // Đảm bảo vite có thể lắng nghe từ mọi nơi trong Docker
+    host: '0.0.0.0',
+    port: 5173,
+  }
 })
